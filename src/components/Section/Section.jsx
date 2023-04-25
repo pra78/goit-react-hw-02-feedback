9e0486672fa240155921fd794c0e8f43d572a3c4
pra78 <66877961+pra78@users.noreply.github.com>
2023-04-25 14:46:47 +0300

    propTypes children

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,5 +1,13 @@
+import PropTypes from 'prop-types';
 import { Container } from './Section.styled';
 
 export const Section = ({ title, children }) => {
     return <Container>{ title }{children}</Container>;
 };
+
+Section.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node
+    ]),
+}
